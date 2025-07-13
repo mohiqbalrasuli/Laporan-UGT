e0fd94191cd1c35b959ec8e2aa474855bd6e6325
mohiqbalrasuli <mohiqbalrasuli@gmail.com>
2025-07-13 08:19:07 +0700

    update

--- resources/views/admin/layout/template_admin.blade.php
+++ resources/views/admin/layout/template_admin.blade.php
@@ -179,7 +179,7 @@
                 <div class="bg-light rounded-top p-4">
                     <div class="row">
                         <div class="col-12 col-sm-6 text-center text-sm-start">
-                            &copy; <a class=" text-success" href="https://alusymuni.ponpes.id/">Pondok Pesantren
+                            &copy; 2025 <a class=" text-success" href="https://alusymuni.ponpes.id/">Pondok Pesantren
                                 Al-Usymuni</a>, All Right Reserved.
                         </div>
                         <div class="col-12 col-sm-6 text-center text-sm-end">
